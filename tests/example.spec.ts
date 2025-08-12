aa7b6bd9a018c41e3c1911b43c50caab83b68633
lethanhduom <thanhduom9@gmail.com>
2025-08-12 20:59:10 +0700

    push ex 01 playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('check title of playwrightvn page', async ({ page }) => {
+  await page.goto('https://material.playwrightvn.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Tài liệu học automation test/);
+});
+
+test('get started link with playwrightvn', async ({ page }) => {
+  await page.goto('https://material.playwrightvn.com/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+
+});
