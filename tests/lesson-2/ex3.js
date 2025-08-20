1bd85a6e7dde422ebf9b3a5e75bdd4949ff2c652
lethanhduom <thanhduom9@gmail.com>
2025-08-20 21:09:56 +0700

    chore: fix format code

--- tests/lesson-2/ex3.js
+++ tests/lesson-2/ex3.js
@@ -2,11 +2,11 @@
 const a = 22;
 // b.
 if (a > 0) {
-    console.log("giá trị bạn nhập là số dương");
+  console.log("giá trị bạn nhập là số dương");
 }
 if (a < 0) {
-    console.log("giá trị bạn nhập là số âm");
+  console.log("giá trị bạn nhập là số âm");
 }
 if (a === 0) {
-    console.log("giá trị bạn nhập là số 0");
+  console.log("giá trị bạn nhập là số 0");
 }
--- tests/lesson-2/ex4.js
+++ tests/lesson-2/ex4.js
@@ -2,12 +2,20 @@
 let height = 170;
 // b.
 let temp = 0;
-if (height > 100){
-temp = height % 100;
-console.log("can nang ly tuong la: "+ (temp*9)/10 + " kg" + ", can nang toi da la: " + temp +
-" kg" + ", can nang toi thieu la: " + (temp*8)/10 + " kg");
+if (height > 100) {
+  temp = height % 100;
+  console.log(
+    "can nang ly tuong la: " +
+      (temp * 9) / 10 +
+      " kg" +
+      ", can nang toi da la: " +
+      temp +
+      " kg" +
+      ", can nang toi thieu la: " +
+      (temp * 8) / 10 +
+      " kg"
+  );
 }
-if (height <= 100){
-    console.log("Chiều cao phải > 100cm để áp dụng công thức trong đề.")
+if (height <= 100) {
+  console.log("Chiều cao phải > 100cm để áp dụng công thức trong đề.");
 }
-
